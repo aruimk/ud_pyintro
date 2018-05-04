8b135c9b76202d577c528b00f663cefb67a206ac
aruimk <miurakimihiro@gmail.com>
2018-05-05 00:10:25 +0900

    add again

---
+++ lesson8.py
@@ -0,0 +1,15 @@
+s = 'My name is Mike. Hi Mike.'
+print(s)
+
+
+is_start = s.startswith('My')
+print(is_start)
+is_start = s.startswith('X')
+print(is_start)
+
+print('################')
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
