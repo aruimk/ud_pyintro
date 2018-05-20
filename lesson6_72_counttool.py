cab415626f72e86d91c6a9e7946e9f7dd5020ebe
aruimk <miurakimihiro@gmail.com>
2018-05-20 23:33:46 +0900

    ほかの方法の追加

--- lesson6_72_counttool.py
+++ lesson6_72_counttool.py
@@ -9,4 +9,23 @@ for c in s:
         d[c] = 0
     d[c] += 1
 
+print('d')
 print(d)
+
+
+
+e = {}
+for c in s:
+    e.setdefault(c,0)
+    e[c] += 1
+print('e')
+print(e)
+
+
+from collections import defaultdict
+f = defaultdict(int)
+for c in s:
+    f[c] += 1
+print('f')
+print(f)
+
